56d4a9557cea087bf8b65976d5af8beb2c4fd43d
eric.neunaber <eric.neunaber@asolutions.com>
2014-09-05 14:49:25 -0500

    initial commit

---
+++ SchedulingAppCore/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingAppCore
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public Team HomeTeam { get; set; }
+        public Team AwayTeam { get; set; }
+    }
+}
